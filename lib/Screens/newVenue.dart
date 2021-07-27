b9f7987662c19ae3dd8775b056404be83de96940
Tezza27 <terryrees@hotmail.com>
2021-07-27 20:35:48 +0100

    Bottom bar with buttons with stack

--- lib/Screens/newVenue.dart
+++ lib/Screens/newVenue.dart
@@ -16,58 +16,81 @@ class _NewVenueScreenState extends State<NewVenueScreen> {
     // final selectedVenue = Provider.of<VenueProvider>(context).findVenueById(selectedVenueId);
     return Scaffold(
       backgroundColor: Theme.of(context).accentColor,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Theme.of(context).accentColor,
-            pinned: true,
-            floating: true,
-            snap: true,
-            expandedHeight: 200.0,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text(
-                "Add New Venue",
-                style: TextStyle(color: Theme.of(context).accentColor),
-              ),
-              centerTitle: true,
-              background: DecoratedBox(
-                position: DecorationPosition.foreground,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.center,
-                      colors: <Color>[
-                        Theme.of(context).primaryColor,
-                        Colors.transparent,
-                      ]),
-                ),
-                child: Image.asset(
-                  'assets/images/piano_bar_harrahs.png',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          NewScrollButtonList(),
-          NewContent(),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                color: Theme.of(context).primaryColor,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text("Cancel"),
-                      ),
-                      ElevatedButton(onPressed: () {}, child: Text("Save")),
-                    ],
+      body: Stack(
+        children: <Widget>[
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                backgroundColor: Theme.of(context).accentColor,
+                pinned: true,
+                floating: true,
+                snap: true,
+                expandedHeight: 200.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text(
+                    "Add New Venue",
+                    style: TextStyle(color: Theme.of(context).accentColor),
                   ),
+                  centerTitle: true,
+                  background: DecoratedBox(
+                    position: DecorationPosition.foreground,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.center,
+                          colors: <Color>[
+                            Theme.of(context).primaryColor,
+                            Colors.transparent,
+                          ]),
+                    ),
+                    child: Image.asset(
+                      'assets/images/piano_bar_harrahs.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              NewScrollButtonList(),
+              NewContent(),
+              // SliverFillRemaining(
+              //   hasScrollBody: false,
+              //   child: Align(
+              //     alignment: Alignment.bottomCenter,
+              //     child: Container(
+              //       color: Theme.of(context).primaryColor,
+              //       child: Padding(
+              //         padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              //         child: Row(
+              //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //           children: <Widget>[
+              //             ElevatedButton(
+              //               onPressed: () {},
+              //               child: Text("Cancel"),
+              //             ),
+              //             ElevatedButton(onPressed: () {}, child: Text("Save")),
+              //           ],
+              //         ),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Theme.of(context).primaryColor,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Cancel"),
+                    ),
+                    ElevatedButton(onPressed: () {}, child: Text("Save")),
+                  ],
                 ),
               ),
             ),
