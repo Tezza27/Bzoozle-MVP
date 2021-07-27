6829407cdf7e52e3fe9f6421dc7f2ad7273a97f3
Alcwyn Parker <alcwyn@buzzinteractive.co.uk>
2021-07-27 21:25:34 +0100

    fix: flutter keyboard action now functioning but needs attention.

--- lib/Widgets/newVenueScreenWidgets/newDescription.dart
+++ lib/Widgets/newVenueScreenWidgets/newDescription.dart
@@ -1,6 +1,7 @@
 import 'package:bzoozle/Lists/hostbuildings.dart';
 import 'package:bzoozle/Lists/venueTypeLists.dart';
 import 'package:flutter/material.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
 
 class NewDescription extends StatefulWidget {
   const NewDescription({Key? key}) : super(key: key);
@@ -15,6 +16,103 @@ class _NewDescriptionState extends State<NewDescription> {
   String? dropdownTypeValue;
   String? dropdownThemeValue;
   String? dropdownHostValue;
+
+  final FocusNode _nodeText1 = FocusNode();
+  final FocusNode _nodeText2 = FocusNode();
+  final FocusNode _nodeText3 = FocusNode();
+  final FocusNode _nodeText4 = FocusNode();
+  final FocusNode _nodeText5 = FocusNode();
+  final FocusNode _nodeText6 = FocusNode();
+
+  /// Creates the [KeyboardActionsConfig] to hook up the fields
+  /// and their focus nodes to our [FormKeyboardActions].
+  KeyboardActionsConfig _buildConfig(BuildContext context) {
+    return KeyboardActionsConfig(
+      keyboardActionsPlatform: KeyboardActionsPlatform.ALL,
+      keyboardBarColor: Colors.grey[200],
+      nextFocus: true,
+      actions: [
+        KeyboardActionsItem(
+          focusNode: _nodeText1,
+        ),
+        KeyboardActionsItem(focusNode: _nodeText2, toolbarButtons: [
+          (node) {
+            return GestureDetector(
+              onTap: () => node.unfocus(),
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.close),
+              ),
+            );
+          }
+        ]),
+        KeyboardActionsItem(
+          focusNode: _nodeText3,
+          onTapAction: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Text("Custom Action"),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text("OK"),
+                        onPressed: () => Navigator.of(context).pop(),
+                      )
+                    ],
+                  );
+                });
+          },
+        ),
+        KeyboardActionsItem(
+          focusNode: _nodeText4,
+        ),
+          KeyboardActionsItem(
+          focusNode: _nodeText5,
+          toolbarButtons: [
+            //button 1
+            (node) {
+              return GestureDetector(
+                onTap: () => node.unfocus(),
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "CLOSE",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              );
+            },
+            //button 2
+            (node) {
+              return GestureDetector(
+                onTap: () => node.unfocus(),
+                child: Container(
+                  color: Colors.black,
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "DONE",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              );
+            }
+          ],
+        ),
+        KeyboardActionsItem(
+          focusNode: _nodeText6,
+          footerBuilder: (_) => PreferredSize(
+              child: SizedBox(
+                  height: 40,
+                  child: Center(
+                    child: Text('Custom Footer'),
+                  )),
+              preferredSize: Size.fromHeight(40)),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,7 +184,8 @@ class _NewDescriptionState extends State<NewDescription> {
                 ),
                 Container(
                   child: TextFormField(
-                    textInputAction: TextInputAction.go,
+                    keyboardType: TextInputType.text,
+                    focusNode: _nodeText2,
                     controller: venueDescriptionController,
                     minLines: 5,
                     maxLines: null,
@@ -95,6 +194,13 @@ class _NewDescriptionState extends State<NewDescription> {
                         labelText: 'Venue Description'),
                   ),
                 ),
+                // TextField(
+                //   keyboardType: TextInputType.text,
+                //   focusNode: _nodeText2,
+                //   decoration: InputDecoration(
+                //     hintText: "Input Text with Custom Done Button",
+                //   ),
+                // ),
               ],
             ),
           ),
